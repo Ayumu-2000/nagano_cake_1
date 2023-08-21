373f458f2360862be9fd7517468b0af99ca96aa2
Ayumu-2000 <136280263+Ayumu-2000@users.noreply.github.com>
2023-08-21 09:18:15 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root to: 'homes#top'
+    get 'homes/about' => 'homes#about', as: 'about'
+  end
+  
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
