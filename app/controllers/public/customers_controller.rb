402fe62e500054d8c37dc1b495a5100a6599b405
Ayumu-2000 <136280263+Ayumu-2000@users.noreply.github.com>
2023-08-28 07:22:15 +0000

    update

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -27,7 +27,7 @@ class Public::CustomersController < ApplicationController
   private
 
   def customer_params
-    params.require(:customers).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :email)
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :email)
   end
 
 end
