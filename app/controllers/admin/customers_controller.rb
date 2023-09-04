9518d7704df72189516692279354d6754c99405a
Ayumu-2000 <136280263+Ayumu-2000@users.noreply.github.com>
2023-09-04 14:27:19 +0000

    update

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,29 @@
+class Admin::CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+     @customer = Customer.find(params[:id])
+     if @customer.update(customer_params)
+       redirect_to 
+     else
+       
+     end 
+  end
+  
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'homes#top'
+    resources :customers, only: [:index, :show, :edit, :update]
     resources :items, only: [:new, :show, :index, :edit, :create, :update]
   end
 
