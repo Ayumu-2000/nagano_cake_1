f86e99216de66bf6da868392cd0f38eeb29e0d20
Ayumu-2000 <136280263+Ayumu-2000@users.noreply.github.com>
2023-09-17 13:31:09 +0000

    update

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,36 +2,40 @@ class Public::CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.all
   end
-  
+
   def create
-    @cart_item = Item.find_by(name: params[:item][:name])
-    return if !@item
-    if @item.valid_name?(params[:item][:name]) && (@item.name == true)
-      @cart_item.amount = @cart_item.amount + cart_item.amount
-    else
+    @cart_item = Item.find_by(params[:cart_item][:item_id])
+    #if @cart_item
+      #cart_item.amount = @cart_item.amount + cart_item.amount
+      # @cart_item.update(amount: cart_item)
+      #reediect_to 
+   # else
+      @cart = CartItem.new(ストロングパラメータ)
+      @cart.item_id = アイテムの情報.id
+      @cart.customer_id = current_customer.id
       @cart_item.save
       redirect_to cart_items_path
-    end 
+   # end
   end
-  
+
   def update
-    
+
   end
-  
+
   def destroy
     cart_item = CartItem.find(params[:id])
     cart_item.destroy
     redirect_to cart_items_path
   end
-  
+
   def destroy_all
     @cart_items = CartItem.destroy_all
     current_customer.cart_items.destroy_all
     redirect_to cart_items_path
   end
-  
+
   private
-  
+
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
   end
