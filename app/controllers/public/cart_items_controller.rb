e85336e2e426eba5df77fa5adb19c2d5ad55ae09
Ayumu-2000 <136280263+Ayumu-2000@users.noreply.github.com>
2023-09-18 09:26:07 +0000

    update

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,42 +1,47 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.all
+    #@cart_item = CartItem.find(params[:id])
   end
 
   def create
-    @cart_item = Item.find_by(params[:cart_item][:item_id])
+    @item = Item.find(params[:cart_item][:item_id])
     #if @cart_item
       #cart_item.amount = @cart_item.amount + cart_item.amount
       # @cart_item.update(amount: cart_item)
-      #reediect_to 
+      #reediect_to
    # else
-      @cart = CartItem.new(ストロングパラメータ)
-      @cart.item_id = アイテムの情報.id
-      @cart.customer_id = current_customer.id
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.item_id = @item.id
+      @cart_item.customer_id = current_customer.id
       @cart_item.save
       redirect_to cart_items_path
    # end
   end
 
   def update
-
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update
+    redirect_to cart_items_path
   end
 
   def destroy
     cart_item = CartItem.find(params[:id])
     cart_item.destroy
-    redirect_to cart_items_path
+    @cart_items = CartItem.all
+    render :index
   end
 
   def destroy_all
-    @cart_items = CartItem.destroy_all
-    current_customer.cart_items.destroy_all
-    redirect_to cart_items_path
+    cart_items = CartItem.all
+    #current_customer.cart_items.destroy_all
+    cart_items.destroy_all
+    render :index
   end
 
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :amount)
+    params.require(:cart_item).permit(:customer_id, :item_id, :amount)
   end
 end
