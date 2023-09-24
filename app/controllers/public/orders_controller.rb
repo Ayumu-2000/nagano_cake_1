494d5eb71e0c73c1cd5e8c1b8772dff574bfdaa8
Ayumu-2000 <136280263+Ayumu-2000@users.noreply.github.com>
2023-09-24 08:32:38 +0000

    update

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,9 +3,10 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
   end
 
-  def chech
-    @order = Order.new(order_params)
+  def check
     @cart_items = current_customer.cart_items.all
+    @total = 0
+    @order = Order.new(order_params)
     @order.postal_code = current_customer.postal_code
     @order.address = current_customer.address
     @order.delivery_name = current_customer.first_name + current_customer.last_name
@@ -17,7 +18,20 @@ class Public::OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
-
+    @order.customer_id = current_customer.id
+    @total = 0
+    current_customer.cart_items.each do |cart_item|
+　　  @ordered_item = OrderItem.new
+　　  @ordered_item.order_id =  @order.id
+      @ordered_item.item_id = cart_item.item_id
+      @ordered_item.amount = cart_item.amount
+      @total = @total + cart_item.subtotal
+      @ordered_item.save
+    end #ループ終わり
+    @ordered_item.purchase_price = @total
+    current_member.cart_items.destroy_all
+    @ordered_item.save
+    render :finish
   end
 
   def index
